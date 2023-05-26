89b3593b5eae7d9bc35f58fd5ccdf59ef07b536c
Sowrub mojumder <nazmulhasun7@gmail.com>
2023-05-26 01:11:27 -0700

    use react custom hooks

--- src/Component/Pages/Home/PopulerMenu/PopularMenu.jsx
+++ src/Component/Pages/Home/PopulerMenu/PopularMenu.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../SectionTitle/SectionTitle';
 import MenuItem from '../../Shared/MenuItem/MenuItem';
+import UseHooks from '../../../../Hooks/Usehooks';
 
 const PopularMenu = () => {
-    const [menu, setMenu]=useState([])
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res=>res.json())
-        .then(data=>{
-          const popularItem =  data.filter(item=>item.category==='popular')
-          setMenu(popularItem)
-        })
-    },[])
+    const [menu]=UseHooks();
+    const popular = menu.filter(item=>item.category === 'popular')
+
+    // const [menu, setMenu]=useState([])
+    // useEffect(()=>{
+    //     fetch('menu.json')
+    //     .then(res=>res.json())
+    //     .then(data=>{
+    //       const popularItem =  data.filter(item=>item.category==='popular')
+    //       setMenu(popularItem)
+    //     })
+    // },[])
     return (
         <div>
             <SectionTitle
@@ -20,7 +24,7 @@ const PopularMenu = () => {
             ></SectionTitle>
             <div className="grid md:grid-cols-2 gap-10">
                 {
-                    menu.map(item=> <MenuItem key={item._id} item={item}></MenuItem>)
+                    popular.map(item=> <MenuItem key={item._id} item={item}></MenuItem>)
                 }
             </div>
             <button className="btn btn-outline border-0 border-b-4 mt-4 flex justify-center mb-10 mx-auto items-center">View Full Menu</button>
--- src/Component/Pages/Meno/Menu/Menu.jsx
+++ src/Component/Pages/Meno/Menu/Menu.jsx
@@ -11,16 +11,16 @@ const Menu = () => {
                 <title className="text-xl font-bold font-[Cinzel] text-[#FFFFFF]">BISTRO BOSS | MENU</title>
             </Helmet>
             <MenuCover img={cover} title='OUR MENU' subTitle='Would you like to try a dish?'></MenuCover>
-            <PopularMenu ></PopularMenu>
+            {/* <PopularMenu ></PopularMenu>
             
             <MenuCover img={cover} title='DESSERTS' subTitle='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'></MenuCover>
             <PopularMenu ></PopularMenu>
 
             <MenuCover img={cover} title='PIZZA' subTitle='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'></MenuCover>
             <PopularMenu ></PopularMenu>
-            
+
             <MenuCover img={cover} title='SALADS' subTitle='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'></MenuCover>
-            <PopularMenu ></PopularMenu>
+            <PopularMenu ></PopularMenu> */}
         </div>
     );
 };
